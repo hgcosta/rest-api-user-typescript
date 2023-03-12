f3ecdd0087be753babd906ac18b40666dbf1fec5
hgcosta <hugocosta.devs@gmail.com>
2023-03-12 11:06:20 -0300

    feature:autenticacao de usuario

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,12 +1,37 @@
 import { Request, Response } from "express";
-import usuarioModel from "../models/usuario.model";
+import Usuario from '../models/usuario.model';
+
 
 class UsuarioController {
 
-    public async cadastrar(req:Request,res:Response): Promise<Response>{
-        const usuario = await usuarioModel.create(req.body);
+   
+
+    public async cadastrar(req: Request, res: Response): Promise<Response> {
+        const usuario = await Usuario.create(req.body);
         return res.json(usuario);
     }
+
+    public async autenticar(req: Request, res: Response): Promise<Response> {
+
+        const { nome, senha } = req.body;
+
+        const usuario = await Usuario.findOne({ nome });
+
+        if (!usuario) {
+            return res.status(400).send({ message: 'Usuário não encontrado!'});
+        }
+
+        if (!(await usuario.compareHash(senha))) {
+            return res.status(400).json({ message: 'Senha inválida!' });
+        }
+
+        return res.json({
+            usuario,
+            token: usuario.generateToken()
+        });
+    }
+
+   
 }
 
 export default new UsuarioController();
--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,19 +1,43 @@
-import { model, Schema } from 'mongoose';
+import { Schema, model, Document, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import { UsuarioInterface } from '../interfaces/usuario.interface';
+
+interface UsuarioModel extends UsuarioInterface, Document {
+    compareHash(data: string): Promise<boolean>;
+    generateToken(): string;
+}
+
+// interface UsuarioStatic extends Model<UsuarioModel> {
+//     buscaTodosChat(idUsuario: string): DocumentQuery<UsuarioModel[], UsuarioModel>;
+// }
 
 const UsuarioSchema = new Schema({
-    nome:{
-        type:String,
-        required:true,
+    nome: {
+        type: String,
+        required: true
     },
-    senha:{
-        type:String,
-        required:true,
+    senha: {
+        type: String,
+        required: true
     },
-    avatar:{
-        type:String,
-        required:false,
+    avatar: {
+        type: String,
+        required: false
     }
-
 });
 
-export default model('Usuário', UsuarioSchema); 
+
+
+UsuarioSchema.pre<UsuarioModel>('save', function createAvatar() {
+    const randomId = Math.floor(Math.random() * (1000000)) + 1;
+
+    this.avatar = `https://api.adorable.io/avatars/285/${randomId}.png`;
+});
+
+UsuarioSchema.methods.compareHash = function(hash: string): Promise<boolean> {
+    return bcrypt.compare(hash, this.senha);
+}
+
+
+
+export default model<UsuarioModel>('Usuario', UsuarioSchema);
